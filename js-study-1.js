919041b454c8b7bb6679857a73090406f1bf8530
bogkk <klm_b@outlook.com>
2020-08-08 20:06:03 +0300

    first commit

---
+++ js-study-1.js
@@ -0,0 +1,68 @@
+// task1
+function sumTo(n) {
+    let sum = 0;
+
+    if (n > 1) {
+        for (let i = 1; i <= n; i++) sum += i;
+    } else {
+        for (let i = 1; i >= n; i--) sum += i;
+    }
+
+    return sum;
+}
+
+// task2
+for (let i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 1000);
+}
+
+// task3
+function reverseString(str) {
+    let result = "";
+    for (let i = str.length - 1; i >= 0; i--) result += str[i];
+    return result;
+}
+
+// task4
+function mul(x, y) {
+    if (y === undefined) {
+        return function (y) {
+            return x + y;
+        }
+    }
+
+    return x + y;
+}
+
+// task5
+function isPalindrome(str) {
+    // знаки препинания и пробелы не удаляю, т.к. функция проверяет только слова, а не предложения
+    let string = str.toLowerCase();
+
+    for (let i = 0; i < string.length / 2; i++) {
+        if (string[i] !== string[string.length - 1 - i]) return false;
+    }
+
+    return true;
+}
+
+// task6
+function isAnagram(str1, str2) {
+    if (str1.length !== str2.length) return false;
+    let str2Arr = str2.toLowerCase().split('').sort();
+    return str1.toLowerCase().split('').sort().every((value, index) => value === str2Arr[index]);
+}
+
+// task7
+function fibonacci(n) {
+    let prev = 1;
+    let next = 1;
+    for (let i = 3; i <= n; i++) {
+        let temp = prev + next;
+        prev = next;
+        next = temp;
+    }
+    return next;
+}
